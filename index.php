4fb208dd37e2a0b2ecf48d1a39c71f62bc0a4dd9
osipthomas5448-blip <osipthomas5448@gmail.com>
2026-07-23 04:33:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 ?>
-<<<<<<< HEAD
 <!DOCTYPE html><html lang="en"><head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
